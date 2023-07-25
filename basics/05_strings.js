a87e631b742b70cf6f74668f4039efb5bca23442
sumedh shewale <121716448+sumedhcode@users.noreply.github.com>
2023-07-25 10:42:00 +0000

    strings in javascript

--- basics/05_strings.js
+++ basics/05_strings.js
@@ -1,3 +1,26 @@
 // strings are very important datatype in javascript
-let myName = "Sumedh";
-console.log(myName);
+let myName = "Sumedh       ";
+let lastName = "  Sumedha   ";
+
+// console.log(myName);
+
+// methods and properties of strings
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName.charAt(3));
+// console.log(myName.concat(" Shewale"));
+// console.log(myName.padEnd(10, "."));
+// console.log(myName.padStart(10, "."));
+// console.log(myName.includes("medh"));
+// console.log(myName.indexOf('me'));
+// console.log(myName.endsWith("Su"));
+// console.log(myName.split(""));
+// console.log(myName.slice(1));
+// console.log(myName.replace("Su", "U"));
+// console.log(myName.repeat(3));
+
+// console.log(myName.valueOf());
+// console.log(myName.trimEnd());
+// console.log(myName.normalize());
+// console.log(lastName.trim());
+// console.log(myName.toLocaleLowerCase());
