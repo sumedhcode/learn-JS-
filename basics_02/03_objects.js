b6635646a01033d9b35d7c9a852550d738094d95
sumedh shewale <uxsumedh00@gmail.com>
2023-07-27 18:35:26 +0530

    objects using constructor

--- basics_02/03_objects.js
+++ basics_02/03_objects.js
@@ -2,3 +2,8 @@
 const instaUser = new Object() // here there is just syntatical difference,
 
 instaUser.name = "Sumedh"
+instaUser.age = 20
+instaUser.isLoggedIn = true
+instaUser.email = "sam@gmail.com"
+
+// console.log(instaUser); // => { name: 'Sumedh', age: 20, isLoggedIn: true, email: 'sam@gmail.com' }
