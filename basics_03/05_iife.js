545dd45ec09d6f1c9697e93fd7cc3120566b0ff0
sumedh shewale <uxsumedh00@gmail.com>
2023-07-31 18:00:01 +0530

    modifying the comment

--- basics_03/05_iife.js
+++ basics_03/05_iife.js
@@ -2,7 +2,9 @@
 
 // Sometimes we want our function to execute immediately and dont want to get polluted by the global variables
 
-// also we can create a private scope to work with and also declare private variables and methods which is limited to the IIFE
+/* in IIFE we can also create a private scope to work with 
+and also declare private variables and methods in it 
+which is limited to the IIFE */
 
 (function immediateFunction(){
     // named function
